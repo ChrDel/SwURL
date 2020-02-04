d1cbe04e56d645868e66028240f828792aa3730b
ChrDel <info@easywebdev.com>
2020-02-04 13:32:37 -0800

    Update ImageCacheType.swift
    Storing from downloaded file instead of png representation.

--- Sources/SwURL/RemoteImage/Cache/ImageCacheType.swift
+++ Sources/SwURL/RemoteImage/Cache/ImageCacheType.swift
@@ -10,7 +10,7 @@ import Combine
 import CoreGraphics
 
 public protocol ImageCacheType {
-    func store(image: CGImage, for url: URL)
+    func store(image: CGImage, for url: URL, tempUrl: URL? = nil)
     func image(for url: URL) -> Future<CGImage, ImageLoadError>
 }
 
